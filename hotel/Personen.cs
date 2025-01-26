17a29ee845f61a108f69598053a8c8b219f956ea
Kratos <johanistmid@gmail.com>
2025-01-26 18:56:54 +0100

    personen klasse erstellt

---
+++ hotel/Personen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel
+{
+    class Personen
+    {
+        int id { get; set; }
+        string vorname { get; set; }
+        string nachname { get; set; }
+        DateTime geburtstag { get; set; }
+    }
+}
